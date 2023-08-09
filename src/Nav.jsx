adce629f89015d3e0245b91ff27221262d543776
unknown <youssefhassany22@gmail.com>
2023-08-09 14:40:41 +0300

    added external API and fixed nav

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,15 +5,15 @@ import { FiShoppingCart } from "react-icons/fi";
 const Nav = () => {
   return (
     <nav>
-      <ul className="flex items-center">
+      <ul className="flex items-center gap-5">
         <Link
-          className="inline-block mr-8 text-xl relative before:absolute before:h-1 before:w-0 before:bg-white before:bottom-0 before:duration-200 hover:before:w-full"
+          className="inline-block lg:mr-8 text-xl relative before:absolute before:h-1 before:w-0 before:bg-white before:bottom-0 before:duration-200 hover:before:w-full"
           to={"/"}
         >
           Home
         </Link>
         <Link
-          className="inline-block mr-8 text-xl bg-gray-700 shadow-lg p-2 rounded-full hover:translate-x-1 duration-200"
+          className="inline-block lg:mr-8 text-xl bg-gray-700 shadow-lg p-2 rounded-full hover:translate-x-1 duration-200"
           to={"/cart"}
         >
           <FiShoppingCart />
