f73f605d62af91a94282bc1fc06533db1252e17c
Youssef Hassany <youssefhassany22@gmail.com>
2024-04-19 23:31:10 +0200

    fix localstorage is undefined bug

--- src/utils/local storage/user-local-storage-functions.ts
+++ src/utils/local storage/user-local-storage-functions.ts
@@ -2,11 +2,17 @@ import { UserRole } from "@/types/UserRole";
 import { userKey } from "./user-key";
 
 export const getUserRole = (): UserRole => {
-  if (!localStorage.getItem(userKey)) {
-    localStorage.setItem(userKey, "none");
-  }
+  // to prevent localstorage is undefined error
+  if (typeof window !== "undefined") {
+    if (!localStorage.getItem(userKey)) {
+      localStorage.setItem(userKey, "none");
+    }
 
-  return localStorage.getItem(userKey) as UserRole;
+    return localStorage.getItem(userKey) as UserRole;
+  } else {
+    // Handle the case where localStorage is not available
+    return "none"; // Return a default value
+  }
 };
 
 export const setUserToCustomer = () => {
